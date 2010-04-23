2d11adf0b9e173d3b033a2234dcacb8e8bbba240
Derek Arnold <github@vyr.us>
2010-04-23 07:10:39 -0500

    Defined model.

--- idgit.py
+++ idgit.py
@@ -1,1 +1,33 @@
 import web
+import model
+
+urls = (
+    '/', 'Browse',
+    '/page/(.*)', 'Browse',
+    '/ticket/(.*)', 'Issue',
+    '/project/(.*)', 'Project',
+    '/user/(.*)', 'User',
+)
+
+class Browse:
+
+def GET(self, page):
+        pass
+
+
+class Issue:
+
+    def GET(self, issue_id):
+        pass
+
+
+class Project:
+
+    def GET(self, project_id):
+        pass
+
+
+class User:
+    
+    def GET(self, user_id):
+        pass
